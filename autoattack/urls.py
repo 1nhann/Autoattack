950c7986cc1d95672893bd97851096652a221b8e
1nhann <inhannsec@gmail.com>
2022-06-04 00:32:04 +0800

    update

--- autoattack/urls.py
+++ autoattack/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
 ]
 
 #--------------------------------------------------------------------
